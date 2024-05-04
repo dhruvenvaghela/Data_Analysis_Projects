69dc30c05b262b3767828f252e1c648b9c8e4c5c
Dhruven Vaghela <152161330+dhruvenvaghela@users.noreply.github.com>
2024-05-04 15:33:14 +0530

    Done Changes As Required In Men_var_std.py
    Completed The Required Coding In mean_var__std.py File

--- Data_analysis/Mean-Variance-Standard Deviation Calculator/mean_var_std.py
+++ Data_analysis/Mean-Variance-Standard Deviation Calculator/mean_var_std.py
@@ -1,10 +1,18 @@
 import numpy as np
 
 def calculate(list):
-    var = np.aray(list).reshape(3,3)
-    print(var)
-
-
-
-
-    return var
+    try:
+        var = np.array(list).reshape(3,3)
+        print(var)
+        calculations={}
+        calculations['mean'] = [var.mean(axis=0).tolist(),var.mean(axis=1).tolist(),var.mean()]
+        calculations['variance'] = [var.var(axis=0).tolist(),var.var(axis=1).tolist(),var.var()]
+        calculations['standard deviation'] = [var.std(axis=0).tolist(),var.std(axis=1).tolist(),var.std()]
+        calculations['max'] = [var.max(axis=0).tolist(),var.max(axis=1).tolist(),var.max()]
+        calculations['min'] = [var.min(axis=0).tolist(),var.min(axis=1).tolist(),var.min()]
+        calculations['sum'] = [var.sum(axis=0).tolist(),var.sum(axis=1).tolist(),var.sum()]
+        
+        return calculations
+    
+    except  ValueError:
+        raise ValueError("List must contain nine numbers.") from None
