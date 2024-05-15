1b68214eefe65509cae93a3e1ea8bb637ab9754e
Dhruven Vaghela <152161330+dhruvenvaghela@users.noreply.github.com>
2024-05-15 14:40:48 +0530

    Done
    Code Camp
    Project 3 Done

--- Data_analysis/Medical Data Visualizer/README.md
+++ Data_analysis/Medical Data Visualizer/README.md
@@ -7,9 +7,6 @@ The rows in the dataset represent patients and the columns represent information
 
 
 
-
-
-
 Tasks
 Create a chart similar to examples/Figure_1.png, where we show the counts of good and bad outcomes for the cholesterol, gluc, alco, active, and smoke variables for patients with cardio=1 and cardio=0 in different panels.
 
@@ -41,20 +38,33 @@ By each number in the medical_data_visualizer.py file, add the code from the ass
 Import the data from medical_examination.csv and assign it to the df variable
 Create the overweight column in the df variable
 Normalize data by making 0 always good and 1 always bad. If the value of cholesterol or gluc is 1, set the value to 0. If the value is more than 1, set the value to 1.
+    
 Draw the Categorical Plot in the draw_cat_plot function
+
 Create a DataFrame for the cat plot using pd.melt with values from cholesterol, gluc, smoke, alco, active, and overweight in the df_cat variable.
+
 Group and reformat the data in df_cat to split it by cardio. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
+
 Convert the data into long format and create a chart that shows the value counts of the categorical features using the following method provided by the seaborn library import : sns.catplot()
+
 Get the figure for the output and store it in the fig variable
+
 Do not modify the next two lines
+
 Draw the Heat Map in the draw_heat_map function
+
 Clean the data in the df_heat variable by filtering out the following patient segments that represent incorrect data:
 height is less than the 2.5th percentile (Keep the correct data with (df['height'] >= df['height'].quantile(0.025)))
 height is more than the 97.5th percentile
 weight is less than the 2.5th percentile
 weight is more than the 97.5th percentile
+
 Calculate the correlation matrix and store it in the corr variable
+
 Generate a mask for the upper triangle and store it in the mask variable
+
 Set up the matplotlib figure
+
 Plot the correlation matrix using the method provided by the seaborn library import: sns.heatmap()
+
 Do not modify the next two lines
--- Data_analysis/Medical Data Visualizer/medical_data_visualizer.py
+++ Data_analysis/Medical Data Visualizer/medical_data_visualizer.py
@@ -3,58 +3,58 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-# 1
+# Import data
 df = pd.read_csv('medical_examination.csv')
 
-# 2
-df['weight_index'] = (df.loc[:]['weight'])/(((df.loc[:]['height'])/100)**2)
-df['overweight'] = ((df.weight_index) > 25).astype(int)
+# Add 'overweight' column
+df['overweight'] = (df['weight']/((df['height']/100)**2) > 25).astype(int)
 
-# 3
+# Normalize data by making 0 always good and 1 always bad. If the value of 'cholestorol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
+df[['gluc','cholesterol']] = (df[['gluc','cholesterol']] > 1).astype(int)
 
-
-# 4
+# Draw Categorical Plot
 def draw_cat_plot():
-    # 5
-    df_cat = None
-
-    # 6
-    df_cat = None
-    
-
-    # 7
+    # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke'])
 
 
+    # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the collumns for the catplot to work correctly.
+    #df_cat = None
 
-    # 8
-    fig = None
+    # Draw the catplot with 'sns.catplot()'
+    fig = sns.catplot(data = df_cat, kind='count',  x='variable', hue='value', col='cardio').set(ylabel = 'total').fig
 
 
-    # 9
+    # Do not modify the next two lines
     fig.savefig('catplot.png')
     return fig
 
 
-# 10
+# Draw Heat Map
 def draw_heat_map():
-    # 11
-    df_heat = None
+    # Clean the data
+    df_heat = df[ 
+        ( df['ap_lo'] <= df['ap_hi'] ) & 
+        ( df['height'] >= df['height'].quantile(0.025) ) & 
+        ( df['height'] <= df['height'].quantile(0.975) ) & 
+        ( df['weight'] >= df['weight'].quantile(0.025) ) & 
+        ( df['weight'] <= df['weight'].quantile(0.975) ) 
+    ]
 
-    # 12
-    corr = None
+    # Calculate the correlation matrix
+    corr = df_heat.corr()
 
-    # 13
-    mask = None
-
-
-
-    # 14
-    fig, ax = None
-
-    # 15
-
+    # Generate a mask for the upper triangle
+    mask = np.triu(corr)
 
 
-    # 16
+    # Set up the matplotlib figure
+    fig, ax =  plt.subplots()
+    
+    # Draw the heatmap with 'sns.heatmap()'
+    ax = sns.heatmap(corr, mask=mask, annot=True, fmt='0.1f', square=True)
+
+
+    # Do not modify the next two lines
     fig.savefig('heatmap.png')
     return fig
